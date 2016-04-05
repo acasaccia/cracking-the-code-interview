467ddc58f685f074ab0638e654798da0ca197918
Andrea Casaccia <ANDREACA@amdocs.com>
2016-04-05 17:12:57 +0100

    Exercise 2.4

---
+++ 02-linked-lists/2.4.js
@@ -0,0 +1,39 @@
+// Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes greater
+// than or equal to x. If x is contained within the list, the values of x only need to be after the elements less than x
+// (see below). The partition element x can appear anywhere in the "right" partition; it does not need to appear between
+// the left and the right partitions.
+//
+// EXAMPLE:
+// Input:   3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition 5]
+// Output:  3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+
+var sll = require('./lib/single-linked-list');
+
+
+exports.partition = function(list, pivot) {
+    var left = new sll();
+    var right = new sll();
+    var current = list.head;
+    while (current) {
+        if (current.value < pivot) {
+            if (left.head === null) {
+                left.head = current;
+                left.tail = current;
+            } else {
+                left.tail.next = current;
+                left.tail = current;
+            }
+        } else {
+            if (right.head === null) {
+                right.head = current;
+                right.tail = current;
+            } else {
+                right.tail.next = current;
+                right.tail = current;
+            }
+        }
+        current = current.next;
+    }
+    left.tail.next = right.head;
+    return left;
+};
---
+++ spec/02-linked-lists/2.4.spec.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var sll = require('../../02-linked-lists/lib/single-linked-list');
+var module = require('../../02-linked-lists/2.4');
+
+describe('partition', function(){
+
+    it('should just work™', function(){
+
+        var list = new sll();
+
+        list.append(3);
+        list.append(5);
+        list.append(8);
+        list.append(5);
+        list.append(10);
+        list.append(2);
+        list.append(1);
+
+        expect(list.display()).toBe("3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1");
+
+        var pivot = 5;
+
+        list = module.partition(list, pivot);
+
+        var current = list.head;
+
+        for (var i=0; i<3; i++) {
+            expect(current.value < pivot).toBe(true);
+            current = current.next;
+        }
+
+        for (var i=0; i<4; i++) {
+            expect(current.value >= pivot).toBe(true);
+            current = current.next;
+        }
+
+    });
+
+});
