c1b1eaae2099fd112f378fc995ef2121086720b5
Andrea Casaccia <acasaccia@gmail.com>
2016-04-10 11:12:01 +0100

    Fixed a comment

--- 02-linked-lists/2.4.js
+++ 02-linked-lists/2.4.js
@@ -3,7 +3,7 @@
 // (see below). The partition element x can appear anywhere in the "right" partition; it does not need to appear between
 // the left and the right partitions.
 //
-// EXAMPLE:
+// EXAMPLE
 // Input:   3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition 5]
 // Output:  3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 
