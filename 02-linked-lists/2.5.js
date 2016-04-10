d53f140c0ac370cccb2871d0f6baa3754a988775
Andrea Casaccia <acasaccia@gmail.com>
2016-04-10 12:17:47 +0100

    Exercise 2.5

---
+++ 02-linked-lists/2.5.js
@@ -0,0 +1,62 @@
+// You have two numbers represented by a linked list, where each node contains a single digit. The digit are stored in
+// *reverse* order, such that the 1's digit is at the head of the list . Write a function that adds the two numbers and
+// returns the sum as a linked list.
+//
+// EXAMPLE
+// Input: (7 -> 1 -> 6) + (5 -> 9 -> 2). That is, 617 + 295
+// Output: 2 -> 1 -> 9. That is, 912
+//
+// FOLLOW UP
+// Suppose the digits are stored in forward order. Repeat the above problem.
+//
+// EXAMPLE
+// Input: (6 -> 1 -> 7) + (2 -> 9 -> 5). That is, 617 + 295
+// Output: 9 -> 1 -> 2. That is, 912
+
+
+
+exports.sum_lists = function(list1, list2) {
+
+    var sll = require('./lib/single-linked-list');
+
+    var digit1 = list1.head;
+    var digit2 = list2.head;
+
+    var result = new sll();
+    var carry = 0;
+    var digits_sum;
+
+    while (digit1 !== undefined || digit2 !== undefined) {
+        digits_sum = parseInt(digit1.value) + parseInt(digit2.value) + carry;
+        result.append(digits_sum % 10);
+        carry = Math.floor(digits_sum / 10);
+        digit1 = digit1.next;
+        digit2 = digit2.next;
+    }
+
+    return result;
+
+};
+
+exports.sum_dllists = function(list1, list2) {
+
+    var dll = require('./lib/double-linked-list');
+
+    var digit1 = list1.tail;
+    var digit2 = list2.tail;
+
+    var result = new dll();
+    var carry = 0;
+    var digits_sum;
+
+    while (digit1 !== undefined || digit2 !== undefined) {
+        digits_sum = parseInt(digit1.value) + parseInt(digit2.value) + carry;
+        result.prepend(digits_sum % 10);
+        carry = Math.floor(digits_sum / 10);
+        digit1 = digit1.previous;
+        digit2 = digit2.previous;
+    }
+
+    return result;
+
+};
---
+++ spec/02-linked-lists/2.5.spec.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var sll = require('../../02-linked-lists/lib/single-linked-list');
+var dll = require('../../02-linked-lists/lib/double-linked-list');
+var module = require('../../02-linked-lists/2.5');
+
+describe('sum_lists', function(){
+
+    it('should just work™', function(){
+
+        var list1 = new sll();
+
+        list1.append(7);
+        list1.append(1);
+        list1.append(6);
+
+        var list2 = new sll();
+
+        list2.append(5);
+        list2.append(9);
+        list2.append(2);
+
+        expect(module.sum_lists(list1, list2).display()).toBe("2 -> 1 -> 9");
+
+    });
+
+});
+
+describe('sum_dllists', function(){
+
+    it('should just work™', function(){
+
+        var list1 = new dll();
+
+        list1.append(6);
+        list1.append(1);
+        list1.append(7);
+
+        var list2 = new dll();
+
+        list2.append(2);
+        list2.append(9);
+        list2.append(5);
+
+        expect(module.sum_dllists(list1, list2).display()).toBe("9 -> 1 -> 2");
+
+    });
+
+});
