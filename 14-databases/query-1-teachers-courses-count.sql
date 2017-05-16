ef7e4e66d6016466cece4a305087ab89b93692a8
Andrea Casaccia <andrea@cainthus.com>
2017-05-16 20:32:01 +0300

    SQL warm up

---
+++ 14-databases/query-1-teachers-courses-count.sql
@@ -0,0 +1,16 @@
+﻿SELECT 
+  "Teachers"."TeacherName", COALESCE("TeacherCoursesCount"."count", 0)
+FROM
+  "Teachers" LEFT JOIN
+  (
+  SELECT
+    "Courses"."TeacherID", COUNT("StudentCourses"."StudentID")
+  FROM
+    "Courses" INNER JOIN "StudentCourses"
+  ON
+    "Courses"."CourseID" = "StudentCourses"."CourseID"
+  GROUP BY
+    "Courses"."TeacherID"
+  ) "TeacherCoursesCount"
+ON
+  "Teachers"."TeacherID" = "TeacherCoursesCount"."TeacherID";
---
+++ 14-databases/query-2-students-courses-count.sql
@@ -0,0 +1,8 @@
+﻿SELECT 
+  "Students"."StudentName", COUNT("StudentCourses"."CourseID")
+FROM
+  "Students" LEFT JOIN "StudentCourses"
+ON
+  "Students"."StudentID" = "StudentCourses"."StudentID"
+GROUP BY
+  "Students"."StudentID";
